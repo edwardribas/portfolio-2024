7b0d082cb2dad1b999cac1a90ab69e2263b1b262
edwardribas <ribasedu2005@gmail.com>
2024-04-16 09:11:10 -0300

    local time starting with zero fix

--- components/local-time.tsx
+++ components/local-time.tsx
@@ -8,14 +8,16 @@ export const LocalTime = () => {
   const [localTime, setLocalTime] = useState("00:00 AM")
 
   useEffect(() => {
-    const localInterval = setInterval(() => {
-      const newTime = new Date().toLocaleTimeString("pt-br", {
-        timeZone: "America/Sao_Paulo",
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: true,
-      })
+    const newTime = new Date().toLocaleTimeString("pt-br", {
+      timeZone: "America/Sao_Paulo",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    })
 
+    setLocalTime(newTime)
+
+    const localInterval = setInterval(() => {
       setLocalTime(newTime)
     }, TIME_UPDATE_SECONDS_COUNTDOWN * 1000)
 
